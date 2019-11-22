4cc7079768bbc812552b2576a36079e26ec8cfa1
amougeno <amougeno@S012B-P06.0670086e.ac-strasbourg.fr>
2019-11-22 08:45:43 +0100

    ma vie

--- Eole/src/affichage/Fenêtre.java
+++ Eole/src/affichage/Fenêtre.java
@@ -7,6 +7,7 @@ package affichage;
  * @author BASTIAN
  * modificiation
  * Yolo
+ * Merde
  */
 public class Fenêtre {
 
--- Eole/src/items/Voilier.java
+++ Eole/src/items/Voilier.java
@@ -6,6 +6,7 @@ package items;
 /**
  * @author BASTIAN
  *uguyhfgu
+ *bonjour je suis un programeur la
  */
 public class Voilier {
 
