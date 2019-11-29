bb3858ed26330ce5b5d77e9db269cbc5389137f5
jmunz <jmunz@S012B-P11.0670086e.ac-strasbourg.fr>
2019-11-29 10:50:39 +0100

    regate liste file

--- Eole/src/items/Regate.java
+++ Eole/src/items/Regate.java
@@ -1,4 +1,5 @@
 package items;
+
 /**
  * @author BASTIAN
  * @author jmunz
@@ -53,35 +54,95 @@ public class Regate {
 		this.nbVoilier = nbVoilier;
 	}
 	
-	
-	
-	
+	/**
+	 * @author jmunz
+	 * @param regarde si la file est vide
+	 */
+	public boolean estVide() { 
+		return (premier == null); 
+	}
 	/**
 	 * @author jmunz
 	 * @param ajoute un voilier à la frégate 
 	 */
 	public void ajoutQueue(Voilier v) {
-		
+		Voilier vNouv = new Voilier();
+		if(!estVide()) {
+			dernier.setSuivant(vNouv);
+		}else {
+			premier = vNouv;
+		}	
+		dernier = vNouv;
 	}
 	/**
 	 * @author jmunz
 	 * @param supprime le voillier sélectionné
 	 */
-	public void supprVoilier(Voilier v) {
-		
+	public void supprVoilier(Voilier suppr) {
+		Voilier v = premier;
+		if(!estVide()) {
+			if(premier != suppr) {
+				while(v.getSuivant() != suppr && v.getSuivant() != dernier) {
+					v = v.getSuivant();
+				}
+				if (v == suppr){
+					v.setSuivant(v.getSuivant().getSuivant());
+				}
+			}else {
+			premier = premier.getSuivant();
+			}
+		}
 	}
 	/**
 	 * @author jmunz
 	 * @return une liste avec tous les voiliers
 	 */
 	public String listerTout() {
-		
+		if(!estVide()) {
+			String s = premier.toString();
+			Voilier v = premier;
+			while(v.getSuivant() != null) {
+				v = v.getSuivant();
+				s = s + "\n" + v.toString();
+			}
+			return s;
+			}else {
+				return "Il n'y a pas de voiliers";
+			}
 	}
 	/**
 	 * @author jmunz
 	 * @param vide la régate de tous ses voiliers
 	 */
 	public void vider() {
-		
+		premier = null;
+		dernier = null;
 	}
+	
+	public static void main(String []args) {
+		Voilier v1 = new Voilier();
+		Voilier v2 = new Voilier();
+		Voilier v3 = new Voilier();
+		Voilier v4 = new Voilier();
+		Voilier v5 = new Voilier();
+		Regate r = new Regate();
+		r.ajoutQueue(v1);
+		r.ajoutQueue(v2);
+		r.ajoutQueue(v3);
+		r.ajoutQueue(v4);
+		r.ajoutQueue(v5);
+		r.listerTout();
+		r.supprVoilier(v4);
+		r.listerTout();
+		r.supprVoilier(v1);
+		r.listerTout();
+		r.supprVoilier(v5);
+		r.listerTout();
+		r.vider();
+		r.listerTout();
+	}
+	
+	
+	
+	
 }
